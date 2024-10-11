1a15caffdf480edf7679a5454eb40a33bf4ce490
melvin-rulit <melvin.melvin.rulit@gmail.com>
2024-10-11 17:03:04 +0000

    add docker files

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,4 @@
 use App\Http\Controllers\V1\IndexController;
 use Illuminate\Support\Facades\Route;
 
-//Route::get('/', function () {
-//    return view('welcome');
-//});
-
 Route::get('/', [IndexController::class, 'index'])->name('index');
